15992fa9298f39aef8204b7d4e256d4c1223168b
増田 詢 <sh-masuda@kentem.co.jp>
2022-07-29 14:49:46 +0900

    part1完了。

--- flutter_sample1/lib/main.dart
+++ flutter_sample1/lib/main.dart
@@ -1,115 +1,92 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
+  //アプリが実行されたらまずここから呼ばれる。
+  //return App()　で根源のWidgetを作成
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
+  //StatelessWidgetはbuildメソッドを持ち、Widgetもしくはテキストを返す。
+  // build メソッドでUI構築に必要なWidgetを組み合わせて組んだWidgetツリーをreturn で返す
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const String title = 'Startup Name Generator';
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(title),
+        ),
+        body: const Center(
+          child: RandomWords(),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+
+    //MaterialApp..デザインを簡単にしてくれるクラス
+    // title... タイトル文字。(なくても動く)
+    // home...画面下部のコンテンツ：アプリケーションが表示されたときに最初に表示される
+    // MaterialAppはStatefulWidget
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({super.key});
+
+  //スクロールするたびワードが増えるので動的なUI　Stateful Widget
+  //StatefulWidgetはbuildメソッドを持たず、createstateメソッドを持ち、これがSteteクラスを返す。
+  @override
+  State<RandomWords> createState() => _RandomWordState();
+}
+
+class _RandomWordState extends State<RandomWords> {
+  //StatefulWidgetの実態
+  //Stateの役割... 状態の保持、更新、buildメソッドでWidgetツリーを返す
+
+  final _suggestions = <WordPair>[]; // 単語のペアを保存する配列
+  final _biggerFonts = const TextStyle(fontSize: 18); // フォントサイズの指定
+
+  @override
+  Widget build(BuildContext context) {
+    //リストビュー
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0), // パディング
+      //四方をそれぞれ指定するときはEdgeInsets.only(top: 16.0, right: 16.0, bottom: 16.0, left: 16.0)
+
+      itemBuilder: (context, i) {
+        //リストが奇数行だったら
+        if (i.isOdd) return const Divider(); //仕切り線のウィジェット
+
+        // 以下、偶数時の処理
+        final index = i ~/ 2;
+        //iは仕切り線とワードの数が一緒に数えられている変数なので
+        //indexにワードの数だけを書き出す
+        if (index >= _suggestions.length) {
+          // suggestions... フィールドで宣言されているリスト
+          //リストの最大値まで表示したらリストを10個増やす
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return ListTile(
+          title: Text(_suggestions[index].asPascalCase,
+                      style: _biggerFonts,
+          ),
+          textColor: Colors.white,
+          tileColor: Colors.deepOrange,
+        );
+        //return Text(_suggestions[index].asPascalCase);
+      },
+    );
+
+    //ランダムな文字列を設定したテキストを返す
+    //final wordPair = WordPair.random();
+    //return Text(wordPair.asPascalCase);
   }
 }
